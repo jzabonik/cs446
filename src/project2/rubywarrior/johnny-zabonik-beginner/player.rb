59a24493dde5fc683060d22363ff01ce7a55d2ce
Johnny Zabonik <jzabonik@mymail.mines.edu>
2014-01-20 15:50:37 -0700

    level 6 done

--- src/project2/rubywarrior/johnny-zabonik-beginner/player.rb
+++ src/project2/rubywarrior/johnny-zabonik-beginner/player.rb
@@ -1,18 +1,43 @@
 class Player
-  @health = 20
+  MAX_HEALTH = 20
+  FIGHT_THRESHHOLD = 14
+  @health = MAX_HEALTH
+  @hit_wall = false
   def play_turn(warrior)
-    # add your code here
-	if warrior.feel.empty?
-		if warrior.health > 18 || @health > warrior.health
-			warrior.walk!
+	if  @hit_wall
+		#walk forward
+		if warrior.feel.empty?
+			if warrior.health == MAX_HEALTH || @health > warrior.health
+				if @health > warrior.health && warrior.health < FIGHT THRESHHOLD
+					warrior.walk!(:backward)
+				else
+					warrior.walk!
+				end
+			else
+				warrior.rest!
+			end
 		else
-			warrior.rest!
+			if warrior.feel.captive?
+				warrior.rescue!
+			else
+				warrior.attack!
+			end
 		end
 	else
-		if warrior.feel.captive?
-			warrior.rescue!
+		if warrior.feel(:backward).wall?
+			@hit_wall = true
+		elsif warrior.feel(:backward).empty?
+			if warrior.health > 18 || @health > warrior.health
+				warrior.walk!(:backward)
+			else
+				warrior.rest!
+			end
 		else
-			warrior.attack!
+			if warrior.feel(:backward).captive?
+				warrior.rescue!(:backward)
+			else
+				warrior.attack!(:backward)
+			end
 		end
 	end
 	@health = warrior.health
